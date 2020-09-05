bb2678863637c813db5285a3ab8a0f3f3f8d79af
Kale <460412008@qq.com>
2020-09-06 03:30:24 +0800

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -8,7 +8,7 @@ set-executionpolicy remotesigned -scope currentuser
 $env:SCOOP='D:\Scoop'
 [environment]::setEnvironmentVariable('SCOOP',$env:SCOOP,'User')
 
-$env:SCOOP_GLOBAL='D:\Scoop\Global'
+$env:SCOOP_GLOBAL='D:\Scoop\global'
 [environment]::setEnvironmentVariable('SCOOP_GLOBAL',$env:SCOOP_GLOBAL,'Machine')
 
 iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
@@ -35,42 +35,42 @@ scoop config aria2-min-split-size 3M
 
 ### 安装底层基础包
 
-scoop install windowsterminal-np
+scoop install windowsterminal-np # ✔
 scoop install terminus
 
-scoop install my/keymap-replacer
+scoop install my/keymap-replacer # ✔
 scoop install my/bandizip
-scoop install my/clover
+scoop install my/clover # ✔
 
 ### 安装必选包
 
-scoop install googlechrome
-scoop install cmder-full
-scoop install sublime-text
-scoop install github
+scoop install googlechrome # ✔
+scoop install cmder-full # ✔
+scoop install sublime-text # ✔
+scoop install github # ✔
 
 ### 安装非必选包
 
-scoop install ditto
-scoop install snipaste
+scoop install ditto # ✔
+scoop install snipaste # ✔
 scoop install wgestures
 
 scoop install freedownloadmanager
-scoop install geekuninstaller
-scoop install autohotkey-installer
+scoop install geekuninstaller # ✔
+scoop install autohotkey-installer # ✔
 
-scoop install screentogif
-scoop install licecap
+scoop install screentogif # ✔
+scoop install licecap # ✔
 
-scoop install postman
-scoop install typora
+scoop install postman # ✔
+scoop install typora # ✔
 
 ### 安装IDE
 
-scoop install oraclejdk
+scoop install oraclejdk # ✔
 scoop install IntelliJ-IDEA-portable
 
-scoop install adb
+scoop install adb # ✔
 scoop install android-sdk
 
 scoop install scrcpy
@@ -83,30 +83,30 @@ scoop install WebStorm-portable
 
 ### 安装自定义bucket中必选包
 
-scoop install my/quicklook
+scoop install my/quicklook # ✔
 scoop install my/listary
 
-scoop install my/caps-unlocker
+scoop install my/caps-unlocker # ✔
 scoop install my/mouse-inc
 
 scoop install my/yx-calendar
 scoop install my/xyplorer
 
 scoop install my/huochat
-scoop install my/ahk-scripts
+scoop install my/ahk-scripts # ✔
 
 ### 安装非必选包
 
 scoop install my/wechat
 scoop install dorado/dingtalk
-scoop install my/open-hashtab
+scoop install my/open-hashtab # ✔
 
-scoop install my/paper-icons
-scoop install my/noMeiryoUI
-scoop install my/mactype
+scoop install my/paper-icons # ✔
+scoop install my/noMeiryoUI # ✔
+scoop install my/mactype # ✔
 
-scoop install my/thunder-light
-scoop install my/thunderx
+scoop install my/thunder-light # ✔
+scoop install my/thunderx # ✔
 
 
 ### 配置环境信息
@@ -121,5 +121,5 @@ scoop install my/process-hacker
 
 ### 提权安装字体
 
-sudo scoop install my/YaHeiMonacoHybrid my/SourceCodePro-Lite -g
-sudo scoop install SarasaGothic-SC SourceCodePro-NF -g
+sudo scoop install my/YaHeiMonacoHybrid my/SourceCodePro-Lite -g # ✔
+sudo scoop install SarasaGothic-SC SourceCodePro-NF -g # ✔
